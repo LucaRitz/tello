9cfe7b42babe7554691abcfe07fddae28671ba7d
LucaRitz <ritz.luca@gmail.com>
2020-04-24 15:32:31 +0200

    Changed compiled lib-location.

--- cmake/Findtello_lib.cmake
+++ cmake/Findtello_lib.cmake
@@ -1,7 +1,7 @@
 add_library(tello_lib STATIC IMPORTED)
 set_target_properties(tello_lib PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${tello_SOURCE_DIR}/include"
-        IMPORTED_LOCATION "${tello_BINARY_DIR}/tello.lib"
+        IMPORTED_LOCATION "${tello_BINARY_DIR}/lib/tello.lib"
         )
 
 add_library(spdlog_lib STATIC IMPORTED)
